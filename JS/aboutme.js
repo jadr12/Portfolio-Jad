a4de9ea80976d30713d36c6add746ad92e21d6d0
Jad Riahi <jad.riahi12@gmail.com>
2018-11-23 17:09:57 +0100

    Cinqieme sauvegarde

---
+++ JS/aboutme.js
@@ -0,0 +1,57 @@
+let all = document.getElementById("btn1")
+let langage = document.getElementById("btn2")
+let design = document.getElementById("btn3")
+
+
+//Bouton All
+
+all.addEventListener("click",function(){
+    var tous = document.querySelectorAll(".skills")
+    
+    for ( var i =0; i <tous.length; i++){
+        document.querySelectorAll(".skills")[i].style.display = "initial";
+       
+    }
+})
+
+
+// Bouton Langage
+
+langage.addEventListener("click", function () {
+   
+   let skillslangage = document.querySelectorAll(".skills");
+
+   for (var i = 0; i < skillslangage.length ; i++) {
+      
+      if (skillslangage[i].classList.contains("langage")) {
+         document.querySelectorAll(".skills")[i].style.display="block";
+      } 
+      else {
+         document.querySelectorAll(".skills")[i].style.display="none"; }
+   }
+})
+
+
+
+
+
+
+// Bouton Design
+
+design.addEventListener("click", function () {
+   
+   let skillsDESIGN = document.querySelectorAll(".skills");
+
+   for (var i = 0; i < skillsDESIGN.length ; i++) {
+      
+      if (skillsDESIGN[i].classList.contains("design")) {
+         document.querySelectorAll(".skills")[i].style.display="block";
+      } 
+      else {
+         document.querySelectorAll(".skills")[i].style.display="none"; }
+   }
+})
+
+
+
+//
